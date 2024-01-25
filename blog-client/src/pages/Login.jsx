11fd3fe80ef5e1c0113893dc74089529e263b498
ajay sonere <ajaysonere786@gmail.com>
2024-01-25 11:57:06 +0530

    login page complted

--- blog-client/src/pages/Login.jsx
+++ blog-client/src/pages/Login.jsx
@@ -1,9 +1,51 @@
-
+/* eslint-disable react/no-unescaped-entities */
+import { useState } from "react";
+import { Link } from "react-router-dom";
 
 const Login = () => {
-  return (
-    <div>Login</div>
-  )
-}
+  const [userData, setUserData] = useState({
+    email: "",
+    password: ""
+  });
 
-export default Login
+  const changeInputHandler = (e) => {
+    setUserData((prevState) => {
+      return { ...prevState, [e.target.name]: [e.target.value] };
+    });
+  };
+
+  return (
+    <section className="login">
+      <div className="container">
+        <h2>Sign In </h2>
+        <form className="form login__form">
+          <p className="form__error-message">This is an Error Message </p>
+          <input
+            type="email"
+            placeholder="example@gmail.com"
+            name="email"
+            value={userData.email}
+            onChange={changeInputHandler}
+            autoFocus
+          />
+          <input
+            type="password"
+            placeholder="password"
+            name="password"
+            value={userData.password}
+            onChange={changeInputHandler}
+            autoFocus
+          />
+          <button type="submit" className="btn primary">
+            Login
+          </button>
+        </form>
+        <small>
+          Don't have an account ? <Link to="/register">sing up</Link>
+        </small>
+      </div>
+    </section>
+  );
+};
+
+export default Login;
