9d1115aa231fd6911b5c3ea86d9cdde89523d9ab
ajay sonere <ajaysonere786@gmail.com>
2024-01-28 10:42:04 +0530

    register and login completed

--- blog-server/controllers/userController.js
+++ blog-server/controllers/userController.js
@@ -1,11 +1,52 @@
+import { User } from "../models/userModel.js";
+import HttpError  from "../models/errorModel.js";
+import bcrypt  from 'bcryptjs';
+import jwt from 'jsonwebtoken';
+
+
 
 
 // ============== Register User ===============
 
 // Path = /api/users/register
 
-export const registerUser = (req , res) => {
-   res.send("Register user");
+export const registerUser = async(req , res , next) => {
+   try {
+      const {name , email , password , confirmPassword } = req.body;
+      
+      if(!name || !email || !password || !confirmPassword){
+         return next(new HttpError(`Please Fill all the fileds` , 422)); 
+      }
+      
+      const newEmail = email.toLowerCase();
+      
+      const emailExist = await User.findOne({email : newEmail});
+      
+      if(emailExist){
+         return next( new HttpError(`User already exist` , 422 ));
+      }
+      
+      const trimedPassword = password.trim();
+      
+      if(trimedPassword.length < 6){
+         return next( new HttpError(`Password must be at least 6 characters `, 422 ));
+      }
+      
+      if(password != confirmPassword){
+         return next( new HttpError(`password and confirm password are not same` , 422 ));
+      }
+      
+      const salt = await bcrypt.genSalt(10);
+      
+      const hashedPassword = await bcrypt.hash(trimedPassword , salt);
+      
+      const newUser = await User.create({name , email: newEmail , password : hashedPassword });
+      
+      res.status(201).json(`New user ${newUser.email} registered`);
+      
+   } catch (error) {
+      return next( new HttpError(`Error while registering User` , 422));
+   }
 }
 
 
@@ -13,8 +54,35 @@ export const registerUser = (req , res) => {
 
 // Path = /api/users/login
 
-export const loginUser = (req , res) => {
-    res.send("Login user");
+export const loginUser = async(req , res , next) => {
+    try{
+    
+       const {email , password} =  await req.body;
+       
+       if(!email || !password){
+          return next(new HttpError(`Please fill all the fields ` , 422));
+       }
+       
+       const existingUser = await User.findOne({email});
+       
+       if(existingUser == null){
+          return next(new HttpError(`User does not exist` , 422));
+       }
+       
+       const comparePassword  = await bcrypt.compare(password , existingUser.password);
+       
+       if(!comparePassword) {
+          return next(new HttpError(`email or password is incorrect` , 422));
+       }
+       
+       
+       const token = jwt.sign({id : existingUser.id , name : existingUser.name}, process.env.JWT_KEY , {expiresIn: '5d'});
+       
+       res.status(200).json(token);
+       
+    }catch(error){
+       return next(new HttpError(`Failed to Login`) , 422);
+    }
 };
 
 
